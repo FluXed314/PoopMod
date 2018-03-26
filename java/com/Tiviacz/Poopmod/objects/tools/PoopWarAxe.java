92c0e17b5533b870a007b8eb56d65ff271cfdc6b
FluXed314 <37763973+FluXed314@users.noreply.github.com>
2018-03-26 15:33:17 -0500

    Additional Blocks to be Effective On
    Extended list with all the Vanilla Blocks, I found, that a Vanilla Axe will break faster.

--- java/com/Tiviacz/Poopmod/objects/tools/PoopWarAxe.java
+++ java/com/Tiviacz/Poopmod/objects/tools/PoopWarAxe.java
@@ -19,8 +19,9 @@ public class PoopWarAxe extends ItemTool implements IHasModel
 
 {
 		public static final ToolMaterial poop_war_axe_material = EnumHelper.addToolMaterial("poop_war_axe_material", 0, 350, 7.0F, 2.5F, 10);
-	
-		private static final Set<Block> EFFECTIVE_ON = Sets.newHashSet(new Block[] {Blocks.PLANKS, Blocks.BOOKSHELF, Blocks.LOG, Blocks.LOG2, Blocks.CHEST, Blocks.PUMPKIN, Blocks.LIT_PUMPKIN, Blocks.MELON_BLOCK, Blocks.LADDER, Blocks.WOODEN_BUTTON, Blocks.WOODEN_PRESSURE_PLATE});
+		
+		//Added additional vanilla blocks to EFFECTIVE_ON. -FluXed314
+		private static final Set<Block> EFFECTIVE_ON = Sets.newHashSet(new Block[] {Blocks.PLANKS, Blocks.BOOKSHELF, Blocks.LOG, Blocks.LOG2, Blocks.CHEST, Blocks.PUMPKIN, Blocks.LIT_PUMPKIN, Blocks.MELON_BLOCK, Blocks.LADDER, Blocks.WOODEN_BUTTON, Blocks.WOODEN_PRESSURE_PLATE, Blocks.STANDING_SIGN, Blocks.WALL_SIGN, Blocks.ACACIA_FENCE, Blocks.ACACIA_FENCE_GATE, Blocks.BIRCH_FENCE, Blocks.BIRCH_FENCE_GATE, Blocks.DARK_OAK_FENCE, Blocks.DARK_OAK_FENCE_GATE, Blocks.JUNGLE_FENCE, Blocks.JUNGLE_FENCE_GATE, Blocks.OAK_FENCE, Blocks.OAK_FENCE_GATE, Blocks.SPRUCE_FENCE, Blocks.SPRUCE_FENCE_GATE, Blocks.TRAPDOOR, Blocks.WOODEN_SLAB, Blocks.DOUBLE_WOODEN_SLAB, Blocks.ACACIA_DOOR, Blocks.BIRCH_DOOR, Blocks.DARK_OAK_DOOR, Blocks.JUNGLE_DOOR, Blocks.OAK_DOOR, Blocks.SPRUCE_DOOR, Blocks.JUKEBOX, Blocks.NOTEBLOCK, Blocks.TRAPPED_CHEST, Blocks.ACACIA_STAIRS, Blocks.BIRCH_STAIRS, Blocks.DARK_OAK_STAIRS, Blocks.JUNGLE_STAIRS, Blocks.OAK_STAIRS, Blocks.SPRUCE_STAIRS, Blocks.STANDING_BANNER, Blocks.WALL_BANNER, Blocks.CRAFTING_TABLE, Blocks.CACTUS, Blocks.COCOA, Blocks.CHORUS_FLOWER, Blocks.CHORUS_PLANT, Blocks.BROWN_MUSHROOM_BLOCK, Blocks.RED_MUSHROOM_BLOCK});
     
 			public PoopWarAxe(String name, ToolMaterial material) {
 				super(material, EFFECTIVE_ON);
